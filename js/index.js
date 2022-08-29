e95f07fd418b9d5ce6461c59fa4b5f4244bdb585
Anthony Milton <110333974+ajmilton@users.noreply.github.com>
2022-08-29 04:52:01 -0700

    Added radio buttons for priority selection

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 
 
 var hand = { 'wood': 0, 'brick': 0, 'sheep': 0, 'wheat': 0, 'ore': 0 };
-console.log(hand['wood']);
 
 const resources = Object.keys(hand);
 const buildings = [
@@ -38,7 +37,30 @@ const buildings = [
     }
 ]
 
-//Checks how many times a single building can be built based on cards in hand, returns that value
+// Creates the radio buttons to choose priority for sorting, is run immediately
+function createPriorityRadioBtns(buildings){
+    const btnNames = buildings.map(building => building.name);
+    const priorityBox = document.getElementById('priority-box');
+    for(i in btnNames){
+        btnItem = document.createElement('input');
+        btnItem.type = 'radio';
+        btnItem.id = btnNames[i];
+        btnItem.name = 'priority';
+        btnItem.value = btnNames[i];
+        btnItem.checked = (i==0);
+        btnLabel = document.createElement('label');
+        btnLabel.htmlFor = btnNames[i];
+        btnLabel.innerHTML = btnNames[i]+' ';
+        priorityBox.appendChild(btnLabel);
+        priorityBox.appendChild(btnItem);
+    }
+}
+createPriorityRadioBtns(buildings);
+
+
+
+
+//Checks how many times a single building can be built based on cards in hand, returns that value(number)
 function canBuildTimes(hand, buildingName){
     const building = buildings.find(building => building.name === buildingName);//Find building in buildings with name value equal to buildingName (passed in)
     if(building==false){return 0}; //Return 0 if building doesn't exist
@@ -50,30 +72,48 @@ function canBuildTimes(hand, buildingName){
 }
 
 
-
-
-
-
-/* Simple function, takes string of resource name, then increases that resource value on the form by 1 */
+/* Simple function, takes resource name(string), then increases that resource value on the form by 1 */
 function addResource(resource) {
     document.getElementById(resource).value = Number(document.getElementById(resource).value) + 1; /* Notice: Had to convert string to num */
 }
 
-/* Takes values from form, then creates a string to tell you what cards you have in hand */
-function submitButton() {
-    var cardString = "<b>Your hand is:</b><br>| ";
+
+function handStringBuilder(hand) {
+    // Hand String Builder
+    var handString = "<b>Your hand is:</b><br> ";
+    var handStringList = [];
     for (var resource in hand) {
         hand[resource] = document.getElementById(resource).value;
-        cardString += (hand[resource]+" - "+resource.charAt(0).toUpperCase()+resource.slice(1) + " | "); //Confusing way to capitalize the resource name
+        if(hand[resource] != 0) //If you have a card of that research
+            {handStringList.push(hand[resource]+" "+resource.charAt(0).toUpperCase()+resource.slice(1))}; // Add capitalized string to list of resources
     }
-    var buildNums = basicBuilder(hand);
-    cardString += "<br><br><b>You can build:</b><br>" + 
+    handString += handStringList.join(', '); // Join resource list and format it, then add to the cardString
+
+    return handString;
+}
+
+function buildStringBuilder(hand){
+    //Building String Builder
+    var buildNums = maxBuildingCount(hand);
+    var buildString = "<br><br><b>You can build:</b><br>" + 
     "<img src='.\\img\\builds\\city.png' alt='city'> " + buildNums['City'] + " Cities " + 
     "<img src='.\\img\\builds\\settlement.png' alt='settlement'> " + buildNums['Settlement'] + " Settlements " +
     "<img src='.\\img\\builds\\road.png' alt='road'> " + buildNums['Road'] + " Roads " +
     "<img src='.\\img\\builds\\dcard.png' alt='dcard'> " + buildNums['Development'] + " Dev Cards <br><br>";
 
-    document.getElementById('cardinfo').innerHTML = cardString;
+    return buildString;
+}
+
+/* Takes values from form, then creates a string to tell you what cards you have in hand */
+function submitButton() {
+
+    cardString = handStringBuilder(hand)+buildStringBuilder(hand);
+
+    document.getElementById('cardinfo').innerHTML = cardString; // Display the cardstring
+
+    var priority = document.forms.catanform.priority.value;
+    console.log(priority);
+    getBuildCombination(hand,priority);
 }
 
 /* Resets all the resource input fields, and resets the cardinfo string */
@@ -85,10 +125,15 @@ function clearButton() {
 }
 
 // Takes your hand of resources, runs it through the canBuildTimes method to return an array of how many of each building you can build maximum
-function basicBuilder(hand){
+function maxBuildingCount(hand){
     const buildList = buildings.map(building=>building.name).reduce( // Create array with building names
         (build, name)=>{build[name]=canBuildTimes(hand,name); // For each building name, assign building a value based on canBuildTimes
         return build; // Return the array, complete with building and number of times building is built 
         }, {});
     return buildList;
+}
+
+
+function getBuildCombination(hand,buildFocus){
+    
 }
