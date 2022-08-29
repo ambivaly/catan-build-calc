929ed41958d5d8d78c4ce6d16bb31e9a6e3715d6
Anthony Milton <110333974+ajmilton@users.noreply.github.com>
2022-08-28 22:36:49 -0700

    Redid building calculation functions with => ops

--- js/index.js
+++ js/index.js
@@ -1,6 +1,57 @@
 
 
-var resources = { 'wood': 0, 'brick': 0, 'sheep': 0, 'wheat': 0, 'ore': 0 };
+var hand = { 'wood': 0, 'brick': 0, 'sheep': 0, 'wheat': 0, 'ore': 0 };
+console.log(hand['wood']);
+
+const resources = Object.keys(hand);
+const buildings = [
+    {
+        name: "City",
+        cost: {
+            "wheat": 2,
+            "ore": 3
+            }
+    },
+    {
+        name: "Settlement",
+        cost: {
+            "wood": 1,
+            "brick": 1,
+            "wheat": 1,
+            "sheep": 1
+        }
+    },
+    {
+        name: "Development",
+        cost: {
+            "sheep": 1,
+            "wheat": 1,
+            "ore": 1
+        }
+    },
+    {
+        name: "Road",
+        cost: {
+            "wood": 1,
+            "brick": 1
+        }
+    }
+]
+
+//Checks how many times a single building can be built based on cards in hand, returns that value
+function canBuildTimes(hand, buildingName){
+    const building = buildings.find(building => building.name === buildingName);//Find building in buildings with name value equal to buildingName (passed in)
+    if(building==false){return 0}; //Return 0 if building doesn't exist
+    const resourceFactors = resources.map(resource =>
+        Math.floor(hand[resource] / building.cost[resource])).filter(f=>!isNaN(f)); // Create array with numbers dividing cards in hand by building cost, ignoring resources which don't apply
+    console.log(resourceFactors);
+    const min = resourceFactors.reduce((min, resourceFactor) => min > resourceFactor ? resourceFactor : min); // Look for the smallest number from above array, this is how many times building can be built
+    return min;
+}
+
+
+
+
 
 
 /* Simple function, takes string of resource name, then increases that resource value on the form by 1 */
@@ -10,53 +61,34 @@ function addResource(resource) {
 
 /* Takes values from form, then creates a string to tell you what cards you have in hand */
 function submitButton() {
-    var cardString = "<b>Your resources are:</b><br>| ";
-    for (var resource in resources) {
-        resources[resource] = document.getElementById(resource).value;
-        cardString += (resources[resource]+" - "+resource.toUpperCase() + " | ");
+    var cardString = "<b>Your hand is:</b><br>| ";
+    for (var resource in hand) {
+        hand[resource] = document.getElementById(resource).value;
+        cardString += (hand[resource]+" - "+resource.charAt(0).toUpperCase()+resource.slice(1) + " | "); //Confusing way to capitalize the resource name
     }
-    var buildNums = basicBuilder();
+    var buildNums = basicBuilder(hand);
     cardString += "<br><br><b>You can build:</b><br>" + 
-    "<img src='.\\img\\builds\\city.png' alt='city'> " + buildNums['cities'] + " Cities " + 
-    "<img src='.\\img\\builds\\settlement.png' alt='settlement'> " + buildNums['settlements'] + " Settlements " +
-    "<img src='.\\img\\builds\\road.png' alt='road'> " + buildNums['roads'] + " Roads " +
-    "<img src='.\\img\\builds\\dcard.png' alt='dcard'> " + buildNums['dcards'] + " Dev Cards ";
+    "<img src='.\\img\\builds\\city.png' alt='city'> " + buildNums['City'] + " Cities " + 
+    "<img src='.\\img\\builds\\settlement.png' alt='settlement'> " + buildNums['Settlement'] + " Settlements " +
+    "<img src='.\\img\\builds\\road.png' alt='road'> " + buildNums['Road'] + " Roads " +
+    "<img src='.\\img\\builds\\dcard.png' alt='dcard'> " + buildNums['Development'] + " Dev Cards <br><br>";
+
     document.getElementById('cardinfo').innerHTML = cardString;
 }
 
 /* Resets all the resource input fields, and resets the cardinfo string */
 function clearButton() {
-    for (var resource in resources) {
+    for (var resource in hand) {
         document.getElementById(resource).value = 0
     }
     document.getElementById('cardinfo').innerHTML = "Please select the cards in your hand below"
 }
 
-/* Basic builder, need to change to better formula */
-function basicBuilder() {
-    var buildDict = { 'roads': 0, 'settlements': 0, 'cities': 0, 'dcards': 0 }
-    while (resources['wheat'] >= 2 && resources['ore'] >= 3) {
-        resources['wheat'] -= 2;
-        resources['ore'] -= 3;
-        buildDict['cities'] += 1;
-    }
-    while (resources['wood'] >= 1 && resources['brick'] >= 1 && resources['sheep'] >= 1 && resources['wheat'] >= 1) {
-        resources['wood'] -= 1;
-        resources['brick'] -= 1;
-        resources['sheep'] -= 1;
-        resources['wheat'] -= 1;
-        buildDict['settlements'] += 1;
-    }
-    while (resources['sheep'] >= 1 && resources['wheat'] >= 1 && resources['ore'] >= 1) {
-        resources['sheep'] -= 1;
-        resources['wheat'] -= 1;
-        resources['ore'] -= 1;
-        buildDict['dcards'] += 1;
-    }
-    while (resources['wood'] >= 1 && resources['brick'] >= 1) {
-        resources['wood'] -= 1;
-        resources['brick'] -= 1;
-        buildDict['roads'] += 1;
-    }
-    return buildDict;
+// Takes your hand of resources, runs it through the canBuildTimes method to return an array of how many of each building you can build maximum
+function basicBuilder(hand){
+    const buildList = buildings.map(building=>building.name).reduce( // Create array with building names
+        (build, name)=>{build[name]=canBuildTimes(hand,name); // For each building name, assign building a value based on canBuildTimes
+        return build; // Return the array, complete with building and number of times building is built 
+        }, {});
+    return buildList;
 }
